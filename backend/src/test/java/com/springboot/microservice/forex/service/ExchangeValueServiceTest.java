8170f9b047c266fcd7ac8ce7cfcf02100dff0ef8
Andrei <109a@mail.ru>
2021-04-22 16:44:04 +0300

    changed tests

--- backend/src/test/java/com/springboot/microservice/forex/service/ExchangeValueServiceTest.java
+++ backend/src/test/java/com/springboot/microservice/forex/service/ExchangeValueServiceTest.java
@@ -1,5 +1,6 @@
 package com.springboot.microservice.forex.service;
 
+import com.springboot.microservice.CurrencyConversionDto;
 import com.springboot.microservice.forex.entity.ExchangeValue;
 import com.springboot.microservice.forex.repository.ExchangeValueRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,43 +9,50 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.math.BigDecimal;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 class ExchangeValueServiceTest {
 
-    ExchangeValueRepository exchangeValueRepository;
-    ExchangeValueServiceImpl exchangeValueServiceImpl;
+    private ExchangeValueRepository exchangeValueRepository;
+    private ExchangeValueServiceImpl exchangeValueServiceImpl;
+    private CurrencyConversionDto currencyConversionDto;
+    private ExchangeValue exchangeValue;
 
     @BeforeEach
     public void setUp() {
         exchangeValueRepository = Mockito.mock(ExchangeValueRepository.class);
         exchangeValueServiceImpl = new ExchangeValueServiceImpl(exchangeValueRepository);
+        currencyConversionDto = new CurrencyConversionDto(1L,"EUR","RUB",new BigDecimal("1"),
+                new BigDecimal("1"), new BigDecimal("0"), 1);
+        exchangeValue = new ExchangeValue(1L, new BigDecimal("85.00"), "EUR", 1, "RUB");
+        when(exchangeValueRepository.findByFromAndTo("EUR", "RUB")).thenReturn(exchangeValue);
+
     }
 
-//    @DisplayName("Test method getConversionMultiple")
-//    @Test
-//    void getConversionMultipleTest() {
-//
-//        String from = "USD";
-//        String to = "INR";
-//        ExchangeValue expectedExchangeValue = new ExchangeValue(1L, new BigDecimal("65.00"), from, 1, to);
-//        when(exchangeValueRepository.findByFromAndTo("USD", "INR")).thenReturn(expectedExchangeValue);
-//        ExchangeValue actualConversionMultiple = exchangeValueServiceImpl.getConversionMultiple(expectedExchangeValue);
-//        assertEquals(expectedExchangeValue, actualConversionMultiple);
-//    }
+    @DisplayName("Test method getConversionMultiple")
+    @Test
+    void getConversionMultipleTest() {
+        BigDecimal expectedConversionMultiple = exchangeValue.getConversionMultiple();
+        BigDecimal actualConversionMultiple = exchangeValueServiceImpl.setConversionMultiple(currencyConversionDto).getConversionMultiple();
+        assertEquals(expectedConversionMultiple, actualConversionMultiple);
+    }
 
-//    @DisplayName("Test method getConversionMultipleAsync")
-//    @Test
-//    void getConversionMultipleAsyncTest() {
-//        String from = "USD";
-//        String to = "INR";
-//        ExchangeValue expectedExchangeValue = new ExchangeValue(1L, new BigDecimal("65.00"), from, 1, to);
-//        when(exchangeValueRepository.findByFromAndTo("USD", "INR")).thenReturn(expectedExchangeValue);
-//        ExchangeValue actualConversionMultiple = exchangeValueServiceImpl.getConversionMultiple(from, to);
-//        assertEquals(expectedExchangeValue, actualConversionMultiple);
-//
-//
-//    }
+    @DisplayName("Test method getConversionMultipleAsync")
+    @Test
+    void getConversionMultipleAsyncTest() {
+        BigDecimal expectedConversionMultiple = exchangeValue.getConversionMultiple();
+        BigDecimal actualConversionMultiple = null;
+        try {
+            actualConversionMultiple = exchangeValueServiceImpl.setConversionMultipleAsync(currencyConversionDto).get().getConversionMultiple();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        assertEquals(expectedConversionMultiple, actualConversionMultiple);
+    }
+
 }
